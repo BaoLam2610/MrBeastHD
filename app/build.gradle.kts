6091c3bd5189bee80c6fcc8b7d73989d125a18ca
LamBao <baolam.nguyen2610@gmail.com>
2025-03-22 21:37:17 +0700

    Update: Add Hilt
    -   Converted the project to use Kotlin DSL (build.gradle.kts).
    -   Added support for Android Library module.
    -   Added Hilt and its necessary plugins and dependencies.
    -   Added kapt plugin.
    -   Updated the dependencies configuration.
    - Created a new compiler configuration file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -45,4 +47,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    /* Hilt */
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.android.compiler)
+}
+
+/* Hilt: Allow references to generated code*/
+kapt {
+    correctErrorTypes = true
 }
