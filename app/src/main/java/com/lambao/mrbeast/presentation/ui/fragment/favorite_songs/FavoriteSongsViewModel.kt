dc51ad3e4389cb9ee18bb5f2f227dfd39e56bade
LamBao <baolam.nguyen2610@gmail.com>
2025-03-27 16:35:12 +0700

    Add coroutine dispatchers and inject view models
    - Added `CoroutineModule` to provide `IO` and `Default` dispatchers.
    - Added `@IoDispatcher` and `@DefaultDispatcher` qualifiers.
    - Injected dispatchers into `OfflineSongsViewModel`, `OnlineSongsViewModel`, `FavoriteSongsViewModel`, and `PlaySongViewModel`.
    - Updated navigation graph to use correct fragment names and added destination change logging in `MusicActivity`.
    - Added `onSupportNavigateUp` to handle navigation in `MusicActivity`.

--- app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/favorite_songs/FavoriteSongsViewModel.kt
+++ app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/favorite_songs/FavoriteSongsViewModel.kt
@@ -1,8 +1,15 @@
 package com.lambao.mrbeast.presentation.ui.fragment.favorite_songs
 
 import com.lambao.base.presentation.ui.viewmodel.BaseViewModel
+import com.lambao.mrbeast.di.DefaultDispatcher
+import com.lambao.mrbeast.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
 @HiltViewModel
-class FavoriteSongsViewModel : BaseViewModel() {
+class FavoriteSongsViewModel @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : BaseViewModel(ioDispatcher, defaultDispatcher) {
 }
--- app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/play_song/PlaySongViewModel.kt
+++ app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/play_song/PlaySongViewModel.kt
@@ -1,8 +1,15 @@
 package com.lambao.mrbeast.presentation.ui.fragment.play_song
 
 import com.lambao.base.presentation.ui.viewmodel.network.NetworkViewModel
+import com.lambao.mrbeast.di.DefaultDispatcher
+import com.lambao.mrbeast.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
 @HiltViewModel
-class PlaySongViewModel : NetworkViewModel() {
+class PlaySongViewModel @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : NetworkViewModel() {
 }
