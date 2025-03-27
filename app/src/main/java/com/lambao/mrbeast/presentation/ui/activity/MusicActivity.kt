795ddeb9d18bb13d2641a771ec43220cd0024edd
LamBao <baolam.nguyen2610@gmail.com>
2025-03-27 16:35:12 +0700

    Add coroutine dispatchers and inject view models
    - Added `CoroutineModule` to provide `IO` and `Default` dispatchers.
    - Added `@IoDispatcher` and `@DefaultDispatcher` qualifiers.
    - Injected dispatchers into `OfflineSongsViewModel`, `OnlineSongsViewModel`, `FavoriteSongsViewModel`, and `PlaySongViewModel`.
    - Updated navigation graph to use correct fragment names and added destination change logging in `MusicActivity`.
    - Added `onSupportNavigateUp` to handle navigation in `MusicActivity`.

--- app/src/main/java/com/lambao/mrbeast/presentation/ui/activity/MusicActivity.kt
+++ app/src/main/java/com/lambao/mrbeast/presentation/ui/activity/MusicActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.lambao.base.presentation.ui.activity.BaseActivity
+import com.lambao.base.utils.log
 import com.lambao.mrbeast_music.R
 import com.lambao.mrbeast_music.databinding.ActivityMusicBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MusicActivity : BaseActivity<ActivityMusicBinding>() {
+
+    private lateinit var navController: NavController
 
     override fun getLayoutResId() = R.layout.activity_music
 
@@ -21,5 +27,20 @@ class MusicActivity : BaseActivity<ActivityMusicBinding>() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            log("Current destination: ${destination.label}")
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/online_songs/OnlineSongsViewModel.kt
+++ app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/online_songs/OnlineSongsViewModel.kt
@@ -1,8 +1,15 @@
 package com.lambao.mrbeast.presentation.ui.fragment.online_songs
 
 import com.lambao.base.presentation.ui.viewmodel.network.NetworkViewModel
+import com.lambao.mrbeast.di.DefaultDispatcher
+import com.lambao.mrbeast.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
 @HiltViewModel
-class OnlineSongsViewModel : NetworkViewModel() {
+class OnlineSongsViewModel @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : NetworkViewModel() {
 }
