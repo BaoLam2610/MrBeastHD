46d4a000dbf3e772811970be5219e7c1cd4b3cf4
LamBao <baolam.nguyen2610@gmail.com>
2025-03-27 12:49:02 +0700

    Fix: Exclude META-INF files in app/build.gradle.kts
    - Added configuration to exclude META-INF files (AL2.0, LGPL2.1, and gradle incremental annotation processors) from the packaged resources in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,13 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/gradle/incremental.annotation.processors"
+        }
+    }
 }
 
 dependencies {
