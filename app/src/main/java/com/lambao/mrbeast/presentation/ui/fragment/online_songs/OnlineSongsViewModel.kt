533a94cae7a35e6ed5cc19f361fbe65fd0ec8ac2
LamBao <baolam.nguyen2610@gmail.com>
2025-03-28 22:10:05 +0700

    Add empty state handling for online songs
    - Added `layout_empty_data.xml` for displaying empty data message.
    - Updated `fragment_online_songs.xml` to include the empty state layout and bind its visibility to `viewModel.shouldShowEmptyData`.
    - Modified `OnlineSongsViewModel` to include `shouldShowEmptyData` state flow, which combines `screenState` and `_songs` to determine if empty state should be displayed.
    - Updated `OnlineSongsFragment` to observe `songs` and submit the list to the adapter, and to observe `shouldShowEmptyData` to update the visibility of the empty state layout.
    - Added `empty_data` string resource.
    - Added `semibold_24` text style in `themes.xml`.

--- app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/online_songs/OnlineSongsViewModel.kt
+++ app/src/main/java/com/lambao/mrbeast/presentation/ui/fragment/online_songs/OnlineSongsViewModel.kt
@@ -1,6 +1,7 @@
 package com.lambao.mrbeast.presentation.ui.fragment.online_songs
 
-import com.lambao.base.data.Resource
+import androidx.lifecycle.viewModelScope
+import com.lambao.base.presentation.ui.state.ScreenState
 import com.lambao.base.presentation.ui.viewmodel.network.NetworkViewModel
 import com.lambao.mrbeast.di.DefaultDispatcher
 import com.lambao.mrbeast.di.IoDispatcher
@@ -9,7 +10,11 @@ import com.lambao.mrbeast.domain.usecase.GetOnlineSongsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,10 +24,24 @@ class OnlineSongsViewModel @Inject constructor(
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : NetworkViewModel(ioDispatcher, defaultDispatcher) {
 
-    private val _songs = MutableStateFlow<Resource<List<Song>>>(Resource.Loading())
+    private val _songs = MutableStateFlow<List<Song>>(emptyList())
     val songs get() = _songs.asStateFlow()
 
+    private val _shouldShowEmptyData = combine(
+        screenState,
+        _songs
+    ) { screenState, songs ->
+        if (screenState is ScreenState.Success) {
+            songs.isEmpty()
+        } else {
+            false
+        }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
+    val shouldShowEmptyData: StateFlow<Boolean> get() = _shouldShowEmptyData
+
     fun getOnlineSongs() {
-        collectApi(getOnlineSongsUseCase.invoke(), _songs)
+        collectApi(getOnlineSongsUseCase.invoke()) { it ->
+            _songs.emit(it)
+        }
     }
 }
